dde66735a8d318b6111ecb9e27b59f2059084e2b
karankarhale <karanp.karhale@gmail.com>
2025-08-26 00:02:58 +0530

    hilt implementation

--- app/src/main/java/com/example/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppingapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.shoppingapp
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
@@ -11,12 +10,10 @@ import com.example.shoppingapp.data.models.Product
 import com.example.shoppingapp.ui.viewmodel.ViewModelFactory
 import com.example.shoppingapp.ui.recyclerview.ProductListAdapter
 import com.example.shoppingapp.ui.viewmodel.MainViewModel
-import javax.inject.Inject
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var viewModelFactory: ViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +24,8 @@ class MainActivity : AppCompatActivity() {
             onProductClick(it)
         }
         recyclerView.adapter = adapter
-        val appComponent = (application as ShoppingApplication).appComponent
-        appComponent.inject(this)
-        val viewModel: MainViewModel by viewModels {
-            viewModelFactory
-        }
+
+        val viewModel: MainViewModel by viewModels()
 
         viewModel.isLoading.observe(this) {
             it?.let {
