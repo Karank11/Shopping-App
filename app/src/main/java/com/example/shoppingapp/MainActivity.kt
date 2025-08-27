629eae54021cdbe7206e168b17091f5b4ae65f4a
karankarhale <karanp.karhale@gmail.com>
2025-08-27 15:46:46 +0530

    Firebase Crashlytics

--- app/src/main/java/com/example/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppingapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
